bbae13cf863ad98b3334057c23141a7a744c2fc8
Drako-Rexon <abhishek491981@gmail.com>
2025-03-13 12:24:33 +0530

    revived

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wp_status_saver/Provider/bottom_nav_provider.dart';
 import 'package:wp_status_saver/Provider/get_status_provider.dart';
 import 'package:wp_status_saver/Screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MainApp());
+  });
 }
 
 class MainApp extends StatelessWidget {
